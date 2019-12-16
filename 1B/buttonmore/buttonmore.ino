f2b1f1acef413365021da771b578635dad624ef3
itepastra <itepastra@gmail.com>
2019-12-16 15:38:28 +0100

    meer lampjes patronen

--- 1B/buttonmore/buttonmore.ino
+++ 1B/buttonmore/buttonmore.ino
@@ -6,7 +6,7 @@ int LedPin3 = 10;
 int LedPin4 = 9;
 int ButtonPin = 2;
 int versie = 4;
-const int VersieAantal = 5;
+const int VersieAantal = 9;
 
 void setup()
 {
@@ -47,6 +47,23 @@ void loop()
     {
         Serial.println("Versie 4");
         lampjes4();
+    }
+    else if (versie == 5)
+    {
+        Serial.println("Versie 5");
+        lampjes5();
+    }else if (versie == 6)
+    {
+        Serial.println("Versie 6");
+        lampjes6();
+    }else if (versie == 7)
+    {
+        Serial.println("Versie 7");
+        lampjes7();
+    }else if (versie == 8)
+    {
+        Serial.println("Versie 8");
+        lampjes8();
     }
 }
 
@@ -155,5 +172,59 @@ void lampjes4()
         analogWrite(LedPin3, (i + 128) % 256);
         analogWrite(LedPin4, (i + 192) % 256);
         delay(4);
+    }
+}
+
+void lampjes5(){
+    for (int i = 0; i < 512; i++)
+    {
+        analogWrite(LedPin1, i % 256);
+        analogWrite(LedPin2, (i + 128) % 256);
+        analogWrite(LedPin3, (i% 256);
+        analogWrite(LedPin4, (i + 128) % 256);
+        delay(2);
+    }
+}
+
+void lampjes6(){
+    digitalWrite(LedPin1,HIGH);
+    digitalWrite(LedPin3,HIGH);
+    delay(250);
+    digitalWrite(LedPin1,LOW);
+    digitalWrite(LedPin2,HIGH);
+    digitalWrite(LedPin3,LOW);
+    digitalWrite(LedPin4,HIGH);
+    delay(250);
+    digitalWrite(LedPin2,LOW);
+    digitalWrite(LedPin4,LOW);
+
+}
+void lampjes7(){
+    digitalWrite(LedPin1,HIGH);
+    delay(100);
+    digitalWrite(LedPin2,HIGH);
+    delay(100);
+    digitalWrite(LedPin3,HIGH);
+    delay(100);
+    digitalWrite(LedPin4,HIGH);
+    delay(100);
+    digitalWrite(LedPin1,LOW);
+    delay(100);
+    digitalWrite(LedPin2,LOW);
+    delay(100);
+    digitalWrite(LedPin3,LOW);
+    delay(100);
+    digitalWrite(LedPin4,LOW);
+    delay(100);
+}
+
+void lampjes8(){
+    for (int i = 0; i < 512; i++)
+    {
+        analogWrite(LedPin1, i % 256);
+        analogWrite(LedPin2, (i + 128) % 256);
+        analogWrite(LedPin3, (i% 256);
+        analogWrite(LedPin4, (i + 128) % 256);
+        delay(2);
     }
 }
