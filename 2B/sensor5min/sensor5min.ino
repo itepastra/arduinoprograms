6b1840cec53f0b0a1c17338650f886acfca8efba
itepastra <itepastra@gmail.com>
2019-12-18 15:50:43 +0100

    fixed for python reading

--- 2B/sensor5min/sensor5min.ino
+++ 2B/sensor5min/sensor5min.ino
@@ -9,6 +9,7 @@ int  csPin = 10;
 File  myFile;
 int n = 0;
 const unsigned int delayT = 5000;
+const String tab = "\t";
 
 Adafruit_BME280  bme;// This  object  will  control  the  sensor
 float  lastT , lastP , lastH;
@@ -22,15 +23,19 @@ void setup(){
         return;
     }
     Serial.println (" initialization  done .");// Open  the  file  for  WRITING.
+    del = SD.remove("meet.txt")
+    if (del) {
+        Serial.println("old file removed")
+    }
     myFile = SD.open("meet.txt", FILE_WRITE);
 
     // if the  file  opened  okay , WRITE  to it:
     if (myFile) 
     {
-        myFile.println("meting nummer   meettijd     temp     pressure     humidity");
+        myFile.println("#meting\tnummer\tmeettijd\ttemp\tpressure\thumidity");
         myFile.close();
     } else {// if the  file didn ’t open , print  an error:
-        Serial.println("error  opening  meetwaarden.txt");
+        Serial.println("error  opening  meet.txt");
     }// Re-open  the  file  for  reading:
     
     int status = bme.begin(0x76);
@@ -44,24 +49,31 @@ void setup(){
 }
 
 void loop(){
-    n++;
-    Serial.println(n);
     lastT = bme.readTemperature();
     lastP = bme.readPressure();
-    lastH=bme.readHumidity();
+    lastH = bme.readHumidity();
 
-    myFile = SD.open("meetwaarden.txt",FILE_WRITE);
+    myFile = SD.open("meet.txt",FILE_WRITE);
     if (myFile){
+        n++;
+        Serial.println(n);
         myFile.print(n);
+        myFile.print(tab);
         myFile.print(millis());
+        myFile.print(tab);
         myFile.print(lastT);
+        myFile.print(tab);
         myFile.print(lastP);
+        myFile.print(tab);
         myFile.println(lastH);
         myFile.close();
+    } else {
+        Serial.println("file fout");
     }
 
     delay(delayT);
     if (n>60){
+        Serial.println("klaar");
         while (1)
         {
             /* code */
