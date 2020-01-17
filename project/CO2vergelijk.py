37172fe0399a17b38327c0be925c874baba05331
itepastra <itepastra@gmail.com>
2020-01-17 14:41:33 +0100

    nog een plotje want alle plots

---
+++ project/CO2vergelijk.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plotdata(matrix1, matrix2, num):
+    millis1 = matrix1[:, 1]
+    millis2 = matrix2[:, 1]
+    CO21 = matrix1[:, 5]
+    CO22 = matrix2[:, 5]
+    secs1 = millis1 / 1000
+    secs2 = millis2 / 1000
+    fig, ax1 = plt.subplots(figsize=(8, 4), num=num)
+
+    (l1,) = ax1.plot(secs1, CO21, label="CO2 1", color="blue", ls="-")
+    (l2,) = ax1.plot(secs2, CO22, label="CO2 2", color="red", ls="-")
+
+    ax1.set_xlabel("$t$ (s)")
+    ax1.set_ylabel("CO2 concentratie (ppm)")
+    plt.grid()
+    ax1.legend([l1, l2], ["zonder geluidsmuur", "met geluidsmuur"])
+    plt.savefig(num)
+
+
+filepathmet = "metgeluidsmuur.txt"
+filepathzonder = "zondergeluidsmuur.txt"
+
+datamet = np.genfromtxt(filepathmet)
+datazonder = np.genfromtxt(filepathzonder)
+
+plotdata(datazonder, datamet, "beideCO2")
+
+plt.show()
